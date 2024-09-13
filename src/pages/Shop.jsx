5b9407c1de2e11c7f6ddb50ca65fc33399f27960
winnerzempire <hynicekwamboka@gmail.com>
2024-09-13 06:40:55 -0700

    shop

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -13,7 +13,7 @@ const CategoryFilter = () => {
 
   const fetchCategories = async () => {
     try {
-      const response = await axios.get('http://127.0.0.1:8000/api/products/');
+      const response = await axios.get('http://viqtech.co.ke/api/products/');
       setCategories(response.data);
     } catch (error) {
       console.error('Error fetching categories:', error);
