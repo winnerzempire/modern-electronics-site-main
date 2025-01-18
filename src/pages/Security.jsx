08291f259c3599d186501e6e3e6eaaba65ca3ead
winnerzempire <hynicekwamboka@gmail.com>
2025-01-18 03:55:00 -0800

    adds

--- src/pages/Security.jsx
+++ src/pages/Security.jsx
@@ -19,19 +19,13 @@ const Audiovisual = () => {
       <section className="image-text-section">
         <div className="container">
           <div className="image-container">
-            <h2>AUDIO VISUAL</h2>
-            <p>
-              Viqtech stands out as one of Africa’s leading AV equipment and system integrators,
-              offering top-notch solutions that combine premium equipment
-              and expert industry knowledge to empower organizations with efficient service delivery.
-            </p>
+            <h2>SECURIRY $ SAFETY</h2>
           </div>
           <div className="text-container">
             <p>
-              Our cutting-edge systems feature the latest technology, ensuring clear audio and high-quality video.
-              From initial design consultation through deployment and managed services, we collaborate closely with clients,
-              facilitating enhanced efficiency for both individuals and businesses. Our support services extend beyond geographical boundaries,
-              fostering connectivity and unity among organizations, their personnel, and partners.
+            In today’s dynamic and interconnected world, ensuring security and safety within various environments is paramount. 
+            Viqtech specializes in providing cutting-edge ICT solutions tailored to meet the diverse security and safety needs of its clients. 
+            These range from delivering comprehensive solutions in Access Control, Fire Evacuation to Video Surveillance.
             </p>
           </div>
         </div>
@@ -43,17 +37,14 @@ const Audiovisual = () => {
             <img src={meetingrooms} alt="Description of image" />
           </div>
           <div className="text-container">
-            <h2>MEETING ROOMS</h2>
+            <h2>ACCESS CONTROL</h2>
             <p>
-              We facilitate interaction and collaboration in your meeting rooms by providing the ideal environment for video communications and virtual meetings.
-              Our promise is to deliver an efficient experience for your users, transitioning a cramped huddle space into an advanced technological boardroom.
+            We offer advanced access control systems designed to regulate and monitor entry to physical spaces with utmost precision and efficiency.
             </p>
             <p>
-              The modern boardroom has developed dramatically with the advancement of technologies.
-              The boardroom is now all about collaborating with people in different locations, and not just in the local office space.
-              Viqtech facilitates interaction and collaboration in your meeting rooms by providing the ideal environment for video communications and virtual meetings.
-              Our boardroom automation will allow you to have complete control of your boardroom from one central
-              device including, LED screens, video walls, projectors, heating, lighting, network equipment and audio.
+            In its simplest form, access control involves identifying a user based on their credentials and then authorizing 
+            the appropriate level of access once they are authenticated. Passwords, pins, 
+            security tokens and even biometric scans are all credentials commonly used to identify and authenticate a user
             </p>
           </div>
         </div>
@@ -65,14 +56,14 @@ const Audiovisual = () => {
             <img src={videowalls} alt="Description of image" />
           </div>
           <div className="text-container">
-            <h2>VIDEO WALLS</h2>
+            <h2>FIRE EVACUATION</h2>
             <p>
-              Video walls serve as powerful visual communication platforms, and Bakyson leverages advanced technologies to create impactful and immersive displays.
-              Here’s how Viqtech transforms spaces with Video Walls. Viqtech's video walls create immersive visual experiences,
-              provide excellent visibility even in large spaces or areas with high ambient light, offer versatility in content
-              display allowing organizations to showcase a wide range of multimedia content and provide a centrally managed and controled
-              content distribution across multiple displays,
-              ensuring consistent messaging and easy content updates.
+             We deliver comprehensive fire evacuation systems that ensure prompt and orderly evacuation procedures during emergencies. 
+             We provide top-of-the-line fire alarm solutions that ensure the safety and protection of lives and property.
+            </p>
+            <p>
+            Our advanced fire alarm systems are designed to detect fires at their earliest stages, providing early warning and enabling swift response and evacuation. 
+            Our advanced voice evacuation solutions are designed to guide occupants to safety with clear and intelligible voice instructions
             </p>
           </div>
         </div>
@@ -84,38 +75,24 @@ const Audiovisual = () => {
             <img src={meetingrooms} alt="Description of image" />
           </div>
           <div className="text-container">
-            <h2>BGM</h2>
+            <h2>VIDEO SURVEILLANCE</h2>
             <p>
-              At Viqtech, our Background Music solutions are crafted to enhance the atmosphere of any space with seamless, high-quality audio.
-              We offer tailored systems that cater to diverse environments, ensuring an engaging and comfortable auditory experience.
+            While utilizing cutting-edge ICT solutions to monitor and safeguard premises effectively, 
+            Viqtech’s advanced video surveillance systems offer a powerful layer of protection, 
+            providing you with real- time monitoring, recording, and peace of mind.   
             </p>
           </div>
         </div>
       </section>
 
-      <section className="display">
-        <div className="container">
-          <div className="image-container">
-            <img src={displayprojector} alt="Description of image" />
-          </div>
-          <div className="text-container">
-            <h2>DISPAY & PROJECTORS</h2>
-            <p>
-              At Viqtech, our Display & Projectors solutions deliver exceptional visual
-              experiences for various applications. We provide advanced technology that enhances presentations, entertainment,
-              and dynamic visual displays, ensuring high-quality imagery and performance.
-            </p>
-          </div>
-        </div>
-      </section>
+      
 
       <section className="solutions">
         <div className="container">
           <div className="text-solutions">
-            <h2>Audio Visual Solutions We Provide</h2>
+            <h2>Access Control Solutions We Provide</h2>
             <p>
-              We provide advanced AV systems for various applications, including presentations, entertainment,
-              and interactive displays, ensuring high-quality audio and crisp visuals for any setting.
+            Our solutions are designed to enhance security, streamline emergency responses, and maintain a secure environment.
             </p>
           </div>
           <div className='image'>
